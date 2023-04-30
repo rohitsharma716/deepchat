9c3c7b30e9035211879570db36105acd09722e6a
rohitsharma716 <102727841+rohitsharma716@users.noreply.github.com>
2023-04-30 17:51:36 +0530

    add controlar and a router for home

--- deepchat/index.js
+++ deepchat/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const  port = 9000;
+const  port = 8000;
+const HomeControllar = require('../controllars/home_controlars');
+const router = require('../routes');
+
+app.use('/',require('../routes'))
+router.get('/',HomeControllar.home)
 
 
 app.listen(port ,(err,res) => console.log(`this app is running in the port no: ${port}`))
